9e317f66777af05b317c122c9931afe9a51bd2a7
Ari Munandar <arimunandar.dev@gmail.com>
2020-08-27 16:15:32 +0700

    Remove funtion unused

--- StarWars/Apps/Common/AppRouter/AppRoute.swift
+++ StarWars/Apps/Common/AppRouter/AppRoute.swift
@@ -25,69 +25,40 @@ public protocol IAppRouter {
     // MARK: - Get Module Handler
 
     func getModule(module: Module) -> UIViewController?
-    func getModule(module: Module, parameters: [String: Any]) -> UIViewController?
-    
+
     // MARK: - Present Module Handler
 
     func presentModule(module: Module)
     func presentModule(module: Module, parameters: [String: Any])
-    func presentModule(module: Module, type: PresentType)
-    func presentModule(module: Module, onPresented: (() -> Void)?)
-    func presentModule(module: Module, onDismissed: (([String: Any]) -> Void)?)
-    func presentModule(module: Module, parameters: [String: Any], type: PresentType)
-    func presentModule(module: Module, parameters: [String: Any], onPresented: (() -> Void)?)
-    func presentModule(module: Module, parameters: [String: Any], onDismissed: (([String: Any]) -> Void)?)
-    func presentModule(module: Module, type: PresentType, onPresented: (() -> Void)?)
-    func presentModule(module: Module, type: PresentType, onDismissed: (([String: Any]) -> Void)?)
-    func presentModule(module: Module, onPresented: (() -> Void)?, onDismissed: (([String: Any]) -> Void)?)
-    func presentModule(module: Module, parameters: [String: Any], type: PresentType, onPresented: (() -> Void)?)
-    func presentModule(module: Module, parameters: [String: Any], type: PresentType, onDismissed: (([String: Any]) -> Void)?)
-    func presentModule(module: Module, type: PresentType, onPresented: (() -> Void)?, onDismissed: (([String: Any]) -> Void)?)
     func presentModule(module: Module, parameters: [String: Any], type: PresentType, onPresented: (() -> Void)?, onDismissed: (([String: Any]) -> Void)?)
-    
+
     // MARK: - Present View Handler
 
     func presentView(view: UIViewController)
-    func presentView(view: UIViewController, animated: Bool)
-    func presentView(view: UIViewController, completion: (() -> Void)?)
-    func presentView(view: UIViewController, animated: Bool, completion: (() -> Void)?)
 
     // MARK: - Dismiss Handler
 
     func dismiss()
-    func dismiss(animated: Bool)
-    func dismiss(parameters: [String: Any])
-    func dismiss(animated: Bool, parameters: [String: Any])
-    
+
     func popViewController()
-    func popViewController(module: Module?)
-    func popViewController(animated: Bool)
-    func popViewController(parameters: [String: Any])
-    func popViewController(module: Module?, animated: Bool)
-    func popViewController(module: Module?, parameters: [String: Any])
-    func popViewController(animated: Bool, parameters: [String: Any])
-    func popViewController(module: Module?, animated: Bool, parameters: [String: Any])
-    
+
     func popToRoot()
-    func popToRoot(animated: Bool)
-    func popToRoot(parameters: [String: Any])
-    func popToRoot(animated: Bool, parameters: [String: Any])
 }
 
 public class AppRouter: BaseAppRouter {
     public static let share = AppRouter().create()
-    
+
     private func create() -> AppRouter {
         var window: UIWindow?
-        
+
         if let _window = UIApplication.shared.keyWindow {
             window = _window
         } else {
             window = UIWindow(frame: UIScreen.main.bounds)
         }
-        
+
         window?.makeKeyAndVisible()
-        
+
         let router = AppRouter()
         router.window = window
         return router
